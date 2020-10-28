9f3721f428c5175d832a9a374f7f318101c2e9a8
muhark <musashi.jacobsharukawa@gmail.com>
2020-10-28 12:07:46 +0000

    Final test push. Next will be transition to prod.

--- src/end-of-survey.php
+++ src/end-of-survey.php
@@ -113,14 +113,14 @@ $email = "musashi.harukawa@politics.ox.ac.uk";
       <div class="sv-description sv-page__description">
         <p>You have now completed the survey. Thank you for participating!</p>
         <h5 class="payment-code">Click <a href=<?php echo $ccode; ?>>here</a> to be redirected back to prolific and receive payment.</h5>
-        <p>Please bear with me while I verify your responses, but I will strive to ensure that you receive payment within 72 of completing this task.</p>
+        <p>Please bear with me while I verify your responses, but I will try my best to ensure that you receive payment within 72 hours of completing this task.</p>
       </div>
       <h4 class="sv-title sv-page__title">Debrief</h4>
       <div class="sv-description sv-page__description">
         <ul>
-          <li><b>What was the purpose of this experiment?</b><br>The purpose of this experiment was to show whether "micro"-targeted political campaigning makes a difference to elections.</li>
-          <li><b>How does this study show that?</b><br>During this experiment, you were assigned to either the "control" or "treatment" group. The control group was shown a random advertisement (or a neutral advertisement explaining voter eligibility). The control group was given the "optimal" ad based on their answers to the first set of questions, chosen by an machine learning algorithm. By looking at the difference in average Biden opinion between these two groups, we can make claims about "effect" of being targeted.</li>
-          <li><b>I want to know more!</b><br>If you have further questions about the survey, payment, or change your mind regarding consent, please contact me in the email address provided in the <a href="/misc/participant-info.pdf">Participant Information Sheet</a>.</li>
+          <li><b>What was the purpose of this experiment?</b><br>The purpose of this experiment was to show whether "micro"-targeted political campaigning makes a difference to election outcomes. If it does, I plan to use this evidence to make the case for clearer disclosure requirements on advertisers.</li>
+          <li><b>How does this study show that?</b><br>During this experiment, you were assigned to either the "control" or "treatment" group. The control group was shown a random advertisement. The treatment group was given the "optimal" ad based on their answers to the first set of questions, chosen by an machine learning algorithm. By looking at the difference in average outcome between these two groups, we can make claims about "effect" of being targeted.</li>
+          <li><b>I want to know more!</b><br>If you have further questions about the survey, payment, want to hear about the research when it is published, or change your mind regarding consent, please contact me in the email address provided in the <a href="/misc/participant-info.pdf">Participant Information Sheet</a>.</li>
       </div>
     </div>
 
